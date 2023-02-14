eecb39d7ad7a97edf390d29be06fd7718a5d72d4
MaloVer1471 <ig.lager@yandex.ru>
2023-02-14 09:09:25 +0300

    step2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,9 @@ const program = new Command();
 
 program
   .version('0.1.0')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --formft <type>', 'output format', 'stylish')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
 
 program.parse(process.argv); 
